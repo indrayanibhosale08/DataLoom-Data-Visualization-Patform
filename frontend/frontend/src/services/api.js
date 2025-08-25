903b68682a8f2fb68b93f2182262d0fe55c2d3e9
Indrayani Bhosale <142092078+indrayanibhosale08@users.noreply.github.com>
2025-08-25 20:45:34 +0530

    Delete frontend directory

--- frontend/frontend/src/services/api.js
+++
@@ -1,7 +0,0 @@
-// frontend/src/services/api.js
-import axios from 'axios';
-
-const API = axios.create({ baseURL: 'http://localhost:5000' }); // Your backend URL
-
-export const register = (data) => API.post('/api/auth/register', data);
-export const login = (data) => API.post('/api/auth/login', data);
